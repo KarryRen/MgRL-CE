4dd4c8008041b0b786b5a1ea7a10f789e7be4744
KarryRen <KarryRenKai@outlook.com>
2024-04-05 13:36:07 +0800

    add loss

--- exp_elect/elect_config.py
+++ exp_elect/elect_config.py
@@ -15,7 +15,15 @@ MODEL_SAVE_PATH = SAVE_PATH + "trained_models/"  # the saving path of models
 # ************************************************************************************ #
 # ************************************ FOR DATASET *********************************** #
 # ************************************************************************************ #
-UCI_ELECT_DATASET_PATH = ("/Users/karry/KarryRen/Scientific-Projects/2023-SCU-Graduation-Paper"
-                          "/Code/Data/UCI_electricity_dataset/dataset")
+UCI_ELECT_DATASET_PATH = "/Users/karry/KarryRen/Scientific-Projects/2023-SCU-Graduation-Paper/Code/Data/UCI_electricity_dataset/dataset"
 TIME_STEPS = 2
 BATCH_SIZE = 1024
+
+# ************************************************************************************ #
+# ******************************* FOR NET CONSTRUCTING ******************************* #
+# ************************************************************************************ #
+ELECT_GRANULARITY_DICT = {"g1": 1, "g2": 2, "g3": 6, "g4": 24, "g5": 96}  # the granularity dict
+GA_K = 1  # the alignment granularity K
+ENCODING_INPUT_SIZE, ENCODING_HIDDEN_SIZE = 1 * GA_K, 64  # the input and hidden size
+LOSS_REDUCTION, LAMBDA_1, LAMBDA_THETA = "sum", 1.0, 0.001  # loss parameter
+LR = 0.01  # the learning rate
