193eb9a3553e037b4fd2ce2a34a8193df4077bcf
KarryRen <KarryRenKai@outlook.com>
2024-04-06 18:26:27 +0800

    finish cm

--- configs/elect_config.py
+++ configs/elect_config.py
@@ -8,7 +8,7 @@
 # ********************************** BASIC SETTINGS ********************************** #
 # ************************************************************************************ #
 RANDOM_SEED = [0, 42, 3407, 114514][0]  # the random seed
-SAVE_PATH = f"exp_elect_rs_{RANDOM_SEED}/"  # the save path of UCI electricity experiments
+SAVE_PATH = f"exp_elect/rs_{RANDOM_SEED}/"  # the save path of UCI electricity experiments
 LOG_FILE = SAVE_PATH + "log.log"  # the log file path
 MODEL_SAVE_PATH = SAVE_PATH + "trained_models/"  # the saving path of models
 
@@ -23,16 +23,16 @@ BATCH_SIZE = 2048
 # ******************************* FOR NET CONSTRUCTING ******************************* #
 # ************************************************************************************ #
 ELECT_GRANULARITY_DICT = {"g1": 1, "g2": 2, "g3": 6, "g4": 24, "g5": 96}  # the granularity dict
-GA_K = 1  # the alignment granularity K
+GA_K, INPUT_SIZE = 1, 1  # the alignment granularity K & the input size
 ENCODING_INPUT_SIZE, ENCODING_HIDDEN_SIZE = 1 * GA_K, 64  # the input and hidden size
-LOSS_REDUCTION, LAMBDA_1, LAMBDA_THETA = "sum", 1.0, 0.001  # loss parameter
+LOSS_REDUCTION, LAMBDA_1, LAMBDA_THETA = "mean", 1.0, 0.001  # loss parameter
 LR = 0.01  # the learning rate
 
 # ************************************************************************************ #
 # ********************************* FOR NET TRAINING ********************************* #
 # ************************************************************************************ #
 # ---- Train Model ---- #
-EPOCHS = 20
+EPOCHS = 1
 
 # ---- Main metric using to select models ---- #
 MAIN_METRIC = "valid_CORR"
