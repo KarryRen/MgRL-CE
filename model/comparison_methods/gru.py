490cfaf9f33687e13d19788b145963719f40af14
KarryRen <KarryRenKai@outlook.com>
2024-04-06 15:29:32 +0800

    add metrics & rnn framework

---
+++ model/comparison_methods/gru.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/6 15:15
+# @Author  : Karry Ren
+
+""""""
--- model/metrics.py
+++ model/metrics.py
@@ -12,6 +12,42 @@
 import numpy as np
 
 
+def r2_score(y_true: np.ndarray, y_pred: np.ndarray, weight: np.ndarray = None, epsilon: float = 1e-10):
+    """ :math:`R^2` (coefficient of determination) regression score function.
+    :math:`R^2 = 1 - SSR/SST`.
+
+    Best possible score is 1.0, and it can be NEGATIVE (because the model can be arbitrarily worse,
+    it need not actually be the square of a quantity R).
+
+    :param y_true: the label, shape=(num_of_samples)
+    :param y_pred: the prediction, shape=(num_of_samples)
+    :param weight: the weight of label, corr with sample, shape=(num_of_samples) CAN'T BE ALL ZERO !!
+    :param epsilon: the epsilon to avoid 0 denominator
+
+     return:
+        - r2, a number shape=()
+
+    """
+
+    # ---- Step 1. Test the shape and weight ---- #
+    assert y_true.shape == y_pred.shape, f"`y_true`, `y_pred` should have the SAME shape !"
+    assert weight.shape == y_true.shape, f"`weight` should have the SAME shape as y_true&y_pred !"
+    assert np.sum(weight) > 0, f"weight can't be all zero !"
+
+    # ---- Step 2. Compute the SSR & SSE ---- #
+    # compute: SSE = sum(weight * (y - y_hat)^2)), a number, shape=()
+    sse = np.sum(weight * ((y_true - y_pred) ** 2), axis=0, dtype=np.float32)
+    # compute SST = sum((y - y_bar)^2)
+    # - the weighted mean of y_true, shape=(num_of_samples)
+    y_bar = np.sum(weight * y_true, axis=0, keepdims=True) / np.sum(weight, axis=0, keepdims=True)
+    # - compute
+    sst = np.sum(weight * ((y_true - y_bar) ** 2), axis=0, dtype=np.float32)
+
+    # ---- Step 3. Compute and Return r2 = 1 - SSR/SST ---- #
+    r2 = 1 - (sse / (sst + epsilon))
+    return r2
+
+
 def corr_score(y_true: np.ndarray, y_pred: np.ndarray, weight: np.ndarray, epsilon: float = 1e-10):
     """ The Pearson correlation coefficients.
 
@@ -51,7 +87,7 @@ def corr_score(y_true: np.ndarray, y_pred: np.ndarray, weight: np.ndarray, epsil
     sum_y_pred_std = np.sqrt(np.sum(weight * ((y_pred - y_pred_bar) ** 2), axis=0, dtype=np.float32))
     denominator = sum_y_true_std * sum_y_pred_std
 
-    # ---- Step 3. Return CORR score ---- #
+    # ---- Step 3. Compute and Return CORR score ---- #
     corr = numerator / (denominator + epsilon)
     return corr
 
@@ -117,10 +153,12 @@ def mae_score(y_true: np.ndarray, y_pred: np.ndarray, weight: np.ndarray):
 
 
 if __name__ == "__main__":
-    y_true = np.array([1, 2, 3])
-    y_pred = np.array([1, 0, 3])
+    y_true = np.array([1, 3, 3])
+    y_pred = np.array([1, 2, 3])
     weight = np.array([1, 1, 0])
 
+    r2 = r2_score(y_true=y_true, y_pred=y_pred, weight=weight)
+    print("r2 = ", r2)
     corr = corr_score(y_true=y_true, y_pred=y_pred, weight=weight)
     print("corr = ", corr)
     rmse = rmse_score(y_true=y_true, y_pred=y_pred, weight=weight)
