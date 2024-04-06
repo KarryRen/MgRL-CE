3c28faebd7144015823f7adef647083774671192
KarryRen <KarryRenKai@outlook.com>
2024-04-06 20:44:20 +0800

    add data preprocess

--- mg_datasets/datasets_preprocess/elect_preprocess.py
+++ mg_datasets/datasets_preprocess/elect_preprocess.py
@@ -21,12 +21,14 @@ Because we want to make the code clear and beautiful, so we need you to do some 
 During the preprocessing, we wil do the following things:
     1. Set the path.
     2. Intercept the raw elect data for the three-year period `from 2012 to 2014`, totally 1096 days and 105216 15_minutes.
-    3. Exclude the samples with more than 10 days of missing data, and just keep 321 clients, while change the unit of data.
-    4. Split the raw data to Train/Valid/Test and save to `15_minutes.csv` file as following:
+    3. Exclude the samples with more than 1 day of missing data, and just keep 321 clients, while change the unit of data.
+    4. The scale of distribution of electricity data varies GREATLY from one client to another !
+          We need to adjust the scale of different data distributions !
+    5. Split the raw data to Train/Valid/Test and save to `15_minutes.csv` file as following:
         - Train (24 months, 366+365=1096 days, and 70176 rows of data)
         - Valid (6 months, 31+28+31+30+31+30=181 days, and 17376 rows of data)
         - Test (6 months, 31+31+30+31+30+31=184 days, and 17664 rows of data)
-    5. Use the down-granularity algorithm to Compute other granularity (1-hour, 4-hours, 12-hours and 1-day) uci electricity data.
+    6. Use the down-granularity algorithm to Compute other granularity (1-hour, 4-hours, 12-hours and 1-day) uci electricity data.
        And compute the daily label.
 
 All in all, after downloading the file from the web, you need:
@@ -54,7 +56,6 @@ TRAIN_DAYS, VALID_DAYS, TEST_DAYS = 731, 181, 184
 
 # ---- Step 1. Change the `PATH` based on your situation ---- #
 UCI_ELECT_DOWNLOAD_FILE_PATH = "../../../Data/UCI_electricity_dataset/LD2011_2014.txt"
-UCI_ELECT_DOWNLOAD_FIX_FILE_PATH = "../../../Data/UCI_electricity_dataset/LD2011_2014(Fix).txt"
 UCI_ELECT_DATASET_PATH = "../../../Data/UCI_electricity_dataset/dataset"
 print("************************** BEGIN UCI ELECTRICITY DATASET PREPROCESSING **************************")
 
@@ -65,21 +66,31 @@ pd.to_datetime(elect_data["Time"])  # change Time type (format)
 elect_data = elect_data[365 * 96:]  # Intercepts from `2012 to 2014` (cut off the 2011)
 print("************************** FINISH INTERCEPTING **************************")
 
-# ---- Step 3. Exclude the samples with more than 10 days of missing data ---- #
+# ---- Step 3. Exclude the samples with more than 1 day of missing data ---- #
 elect_data_1_day = elect_data.groupby(elect_data.index // 96).transform("sum")  # group sum
 elect_data_1_day["Time"] = elect_data["Time"]  # change time column
 elect_data_1_day = elect_data_1_day[elect_data_1_day.index % 96 == 95]  # just keep 1-day data
-elect_data_1_day = elect_data_1_day[:10]  # 2012-01-01 to 2012-01-10
-keep_clients_list = []
+elect_data_1_day = elect_data_1_day[:1]  # 2012-01-01 to 2012-01-10
+keep_clients_list = []  # the clients list going to keep
 for client in elect_data_1_day.columns[1:]:  # for-loop all clients
-    if np.sum(elect_data_1_day[client].values == 0) < 10:  # no more 10
+    if np.sum(elect_data_1_day[client].values == 0) < 1:  # no more than 1 day
         keep_clients_list.append(client)  # append the right clients
-elect_data = elect_data[["Time"] + keep_clients_list]  # exclude
+elect_data = elect_data[["Time"] + keep_clients_list]  # exclude clients and keep `Time` column of 15 minutes
 print(f"************************** FINISH EXCLUDING AND GET {len(keep_clients_list)} CLIENTS **************************")
-elect_data[keep_clients_list] = elect_data[keep_clients_list] / 4.0  # change the unit of data.
+# change the unit of data (from kW*15min to kW*h)
+elect_data[keep_clients_list] = elect_data[keep_clients_list] / 4.0
 print(f"************************** FINISH CHANGE kW*15min to kW*h **************************")
 
-# ---- Step 4. Split to Train & Valid & Test, and save the 15_minutes.csv ---- #
+# ---- Step 4. Adjust the scale of different data distributions ---- #
+# get the daily elect data of the first day
+elect_data_1_day_of_first_day = elect_data_1_day[["Time"] + keep_clients_list][:1]
+# save for future using
+elect_data_1_day_of_first_day.to_csv(f"{UCI_ELECT_DATASET_PATH}/elect_data_1_day_of_first_day.csv", index=False)
+# change the scale by the first day data
+elect_data[keep_clients_list] = elect_data[keep_clients_list].values / elect_data_1_day_of_first_day[keep_clients_list].values
+print(f"************************** FINISH ADJUSTING SCALE **************************")
+
+# ---- Step 5. Split to Train & Valid & Test, and save the 15_minutes.csv ---- #
 train_elect_data = elect_data[:70176]  # Train (24 months, 366+365=731 days, and 70176 rows of data)
 train_elect_data.to_csv(f"{UCI_ELECT_DATASET_PATH}/Train/15_minutes.csv", index=False)
 valid_elect_data = elect_data[70176:87552]  # Valid (6 months, 31+28+31+30+31+30=181 days, and 17376 rows of data)
@@ -88,7 +99,7 @@ test_elect_data = elect_data[87552:]  # Test (6 months, 31+31+30+31+30+31=184 da
 test_elect_data.to_csv(f"{UCI_ELECT_DATASET_PATH}/Test/15_minutes.csv", index=False)
 print("************************** FINISH 15 MINUTES SPLITTING **************************")
 
-# ---- Step 5. Down-granularity algorithm (1-hour, 4-hours, 12-hours and 1-day) uci electricity data ---- #
+# ---- Step 6. Down-granularity algorithm (1-hour, 4-hours, 12-hours and 1-day) uci electricity data ---- #
 for data_type in ["Train", "Valid", "Test"]:
     print(data_type)
     # Read the 15 minutes data, from the saved `.csv` file
