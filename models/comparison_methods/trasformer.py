eeaf3a770c5ee2ac6d0ab345c41ea242343c15c0
KarryRen <KarryRenKai@outlook.com>
2024-04-15 23:30:22 +0800

    add

--- models/comparison_methods/trasformer.py
+++ models/comparison_methods/trasformer.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2024/4/12 18:18
+# @Time    : 2024/4/15 23:26
 # @Author  : Karry Ren
 
-""" The Comparison Methods 2. LSTM.
+""" The Comparison Methods 3. Transformer.
 
-Ref. https://github.com/microsoft/qlib/blob/main/qlib/contrib/model/pytorch_lstm.py#L286
+Ref. https://github.com/microsoft/qlib/blob/main/qlib/contrib/model/pytorch_transformer.py#L258
 
 """
 
@@ -14,33 +14,33 @@ from torch import nn
 from typing import Dict
 
 
-class LSTMNet(nn.Module):
-    """ The 2 Layer LSTM. hidden_size = 64. """
+class GRUNet(nn.Module):
+    """ The 2 Layer GRU. hidden_size = 64. """
 
     def __init__(
             self, input_size: int, hidden_size: int = 64, num_layers: int = 2,
             dropout: float = 0.0, device: torch.device = torch.device("cpu")
     ):
-        """ The init function of LSTM Net.
+        """ The init function of GRU Net.
 
         :param input_size: input size for each time step
-        :param hidden_size: hidden size of lstm
-        :param num_layers: the num of lstm layers
+        :param hidden_size: hidden size of gru
+        :param num_layers: the num of gru layers
         :param dropout: the dropout ratio
         :param device: the computing device
 
         """
 
-        super(LSTMNet, self).__init__()
+        super(GRUNet, self).__init__()
         self.device = device
         self.input_size = input_size
 
-        # ---- Log the info of LSTM ---- #
-        logging.info(f"|||| Using LSTM Now ! input_size={input_size}, hidden_size={hidden_size}, "
+        # ---- Log the info of GRU ---- #
+        logging.info(f"|||| Using GRU Now ! input_size={input_size}, hidden_size={hidden_size}, "
                      f"num_layers={num_layers}, dropout_ratio={dropout}||||")
 
-        # ---- Part 1. The rnn of the LSTM module ---- #
-        self.rnn = nn.LSTM(
+        # ---- Part 1. The rnn of the GRU module ---- #
+        self.rnn = nn.GRU(
             input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, dropout=dropout
         ).to(device=device)
 
@@ -48,7 +48,7 @@ class LSTMNet(nn.Module):
         self.fc_out = nn.Linear(hidden_size, 1).to(device=device)
 
     def forward(self, mul_granularity_input: Dict[str, torch.Tensor]):
-        """ The forward function of LSTM Net.
+        """ The forward function of GRU Net.
 
         :param mul_granularity_input: the input multi granularity, a dict with the format:
             {
@@ -86,7 +86,7 @@ class LSTMNet(nn.Module):
         return output
 
 
-if __name__ == "__main__":  # A demo of LSTM
+if __name__ == "__main__":  # A demo of GRU
     bath_size, time_steps, D = 16, 4, 1
     mg_input = {
         "g1": torch.ones((bath_size, time_steps, 1, D)),
@@ -98,6 +98,52 @@ if __name__ == "__main__":  # A demo of LSTM
     g_dict = {"g1": 1, "g2": 2, "g3": 6, "g4": 24, "g5": 96}
     dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = LSTMNet(input_size=1, device=dev)
+    model = GRUNet(input_size=1, device=dev)
     out = model(mg_input)
     print(out["pred"].shape)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        # [T, N, F]
+        return x + self.pe[: x.size(0), :]
+
+
+class Transformer(nn.Module):
+    def __init__(self, d_feat=6, d_model=8, nhead=4, num_layers=2, dropout=0.5, device=None):
+        super(Transformer, self).__init__()
+        self.feature_layer = nn.Linear(d_feat, d_model)
+        self.pos_encoder = PositionalEncoding(d_model)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
+        self.decoder_layer = nn.Linear(d_model, 1)
+        self.device = device
+        self.d_feat = d_feat
+
+    def forward(self, src):
+        # src [N, F*T] --> [N, T, F]
+        src = src.reshape(len(src), self.d_feat, -1).permute(0, 2, 1)
+        src = self.feature_layer(src)
+
+        # src [N, T, F] --> [T, N, F], [60, 512, 8]
+        src = src.transpose(1, 0)  # not batch first
+
+        mask = None
+
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, mask)  # [60, 512, 8]
+
+        # [T, N, F] --> [N, T*F]
+        output = self.decoder_layer(output.transpose(1, 0)[:, -1, :])  # [512, 1]
+
+        return output.squeeze()
