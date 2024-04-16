1fe08da55032341ee4df98d770e3c30ec541904b
KarryRen <KarryRenKai@outlook.com>
2024-04-16 16:36:09 +0800

    finish lob preprocess

--- mg_datasets/datasets_preprocess/lob_preprocess/lob_preprocess.py
+++ mg_datasets/datasets_preprocess/lob_preprocess/lob_preprocess.py
@@ -10,7 +10,7 @@ During the preprocessing, we wil do operations by the following steps:
         Computing other granularity data: 1 second, 10 seconds, 30 seconds, 1 minute.
 
 All in all, after downloading the file from the web, you need:
-    1. Change the `PATH` based on your situation.
+    1. Change and Create the `PATH` based on your situation.
 
 """
 
@@ -19,7 +19,8 @@ import numpy as np
 import os
 from price_alignment_features.cal_paf import cal_bid_paf_mat, cal_ask_paf_mat
 
-# ---- TRADING DAYS ---- #
+# ---- Define the Params ---- #
+# define the number of trading dates and tick
 TRADING_DATES_NUM = 242  # totally 242 days
 TICK_NUM = 28800  # the num of total ticks in one day
 # define the gap of each granularity
@@ -33,23 +34,33 @@ LOB_COLUMNS = [
     "BidPrice5", "BidVolume5", "AskPrice5", "AskVolume5"
 ]
 
-# ---- Step 1. Change the `PATH` based on your situation ---- #
+# ---- Step 1. Change and Create the `PATH` based on your situation ---- #
+# the path of download raw data
 LOB_DOWNLOAD_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/download_data"
-assert os.path.exists(LOB_DOWNLOAD_FILE_PATH), f"Please DOWNLOAD the Future LOB dataset following `README.md` !"
+assert os.path.exists(LOB_DOWNLOAD_FILE_PATH), f"Please DOWNLOAD the Future LOB dataset and move it to {LOB_DOWNLOAD_FILE_PATH} !"
+# the path of 0.5 seconds files
 LOB_05_SECOND_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/0.5_seconds"
+assert os.path.exists(LOB_05_SECOND_FILE_PATH), f"Please CREATE the path of 0.5 seconds files {LOB_05_SECOND_FILE_PATH} !"
+# the path of 1 second files
 LOB_1_SECOND_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/1_second"
+assert os.path.exists(LOB_1_SECOND_FILE_PATH), f"Please CREATE the path of 1 second files {LOB_1_SECOND_FILE_PATH} !"
+# the path of 10 seconds files
 LOB_10_SECOND_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/10_seconds"
+assert os.path.exists(LOB_10_SECOND_FILE_PATH), f"Please CREATE the path of 10 seconds files {LOB_10_SECOND_FILE_PATH} !"
+# the path of 30 seconds files
 LOB_30_SECOND_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/30_seconds"
+assert os.path.exists(LOB_30_SECOND_FILE_PATH), f"Please CREATE the path of 30 seconds files {LOB_30_SECOND_FILE_PATH} !"
+# the path of 1 minute files
 LOB_1_MINUTE_FILE_PATH = "../../../../Data/Future_LOB_dataset/IF_M0/1_minute"
+assert os.path.exists(LOB_1_MINUTE_FILE_PATH), f"Please CREATE the path of 1 minute files {LOB_1_MINUTE_FILE_PATH} !"
 
 # ---- Step 2. For-Loop all `.csv` files of 0.5 seconds lob ---- #
-lob_file_list = sorted(os.listdir(LOB_DOWNLOAD_FILE_PATH))  # get the list of 0.5 seconds lob files
-assert len(lob_file_list) == TRADING_DATES_NUM, \
-    f"{TRADING_DATES_NUM} ERROR ! There are{len(lob_file_list)} trading days in lob dataset !"
+lob_file_list = sorted(os.listdir(LOB_DOWNLOAD_FILE_PATH))  # get the list of all raw lob files
+assert len(lob_file_list) == TRADING_DATES_NUM, f"{TRADING_DATES_NUM} ERROR ! There are{len(lob_file_list)} trading days in lob dataset !"
 for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granularity data.
     print(f"***** `{file_idx + 1} | {TRADING_DATES_NUM}` START PROCESSING `{lob_file}` NOW !!! *****")
 
-    # - 2.0 read the raw 0.5 seconds lob data
+    # - 2.0 Read the raw 0.5 seconds lob data
     lob_data_df = pd.read_csv(f"{LOB_DOWNLOAD_FILE_PATH}/{lob_file}")
     # load the price and volume
     bid_price = lob_data_df[["BidPrice1", "BidPrice2", "BidPrice3", "BidPrice4", "BidPrice5"]].values  # shape=(28800, 5)
@@ -63,22 +74,23 @@ for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granulari
     bid_volume = bid_volume.reshape(TICK_NUM, 5, 1)
     ask_volume = ask_volume.reshape(TICK_NUM, 5, 1)
 
-    # - 2.1 get the 0.5 seconds lob data
-    # define the empty 1-second lob data, shape=(28800, 5, 2, 2)
+    # - 2.1 get the 0.5 seconds lob data, keep raw
+    # define the empty 1-second lob data, shape=(28800, 5, 2, 2), (T, L, D, F)
     lob_data_05_second = np.zeros((TICK_NUM, 5, 2, 2))  # reshape to (28800, 20)
     lob_data_05_second[:, :, 0, 0] = bid_price  # bid price
     lob_data_05_second[:, :, 0, 1] = bid_volume[:, :, 0]  # bid volume
     lob_data_05_second[:, :, 1, 0] = ask_price  # ask price
     lob_data_05_second[:, :, 1, 1] = ask_volume[:, :, 0]  # ask volume
-    lob_data_05_second = lob_data_05_second.reshape(TICK_NUM, -1)  #
-    lob_data_05_second_df = pd.DataFrame(lob_data_05_second, columns=LOB_COLUMNS)  # make the df
+    lob_data_05_second = lob_data_05_second.reshape(TICK_NUM, -1)  # reshape to (28800, 20)
+    lob_data_05_second_df = pd.DataFrame(lob_data_05_second, columns=LOB_COLUMNS)  # construct the df
     lob_data_05_second_df.to_csv(f"{LOB_05_SECOND_FILE_PATH}/05_seconds_{lob_file}", index=False)  # save the df
     print(f"   - 1. FINISH 0.5 SECONDS !!!")
 
-    # - 2.2 compute the 1 seconds lob data
-    # define the empty 1-second lob data, shape=(28800/2, 5, 2, 2)
+    # - 2.2 compute the 1 seconds lob data, align by price during 2 ticks and sum volume
+    # define the empty 1-second lob data, shape=(28800/2, 5, 2, 2), (T, L, D, F)
     lob_data_1_second = np.zeros((TICK_NUM // SECOND_1_TICK_GAP, 5, 2, 2))
-    # for loop the tick by `HF` to gen all PA Feature of each tick
+    # for loop the tick by `SECOND_1_TICK_GAP` to gen all PA Feature of each tick
+    tick_i = 0
     for tick_i in range(SECOND_1_TICK_GAP - 1, TICK_NUM, SECOND_1_TICK_GAP):
         # compute the price range and paf-volume
         bid_price_range, bid_paf_volume = cal_bid_paf_mat(
@@ -94,15 +106,17 @@ for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granulari
         lob_data_1_second[tick_i // SECOND_1_TICK_GAP, :, 0, 1] = bid_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
         lob_data_1_second[tick_i // SECOND_1_TICK_GAP, :, 1, 0] = ask_price_range[:5]  # bid price
         lob_data_1_second[tick_i // SECOND_1_TICK_GAP, :, 1, 1] = ask_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
+    assert tick_i == TICK_NUM - 1, "tick_i ERROR !"  # test the tick_i
     lob_data_1_second = lob_data_1_second.reshape(TICK_NUM // SECOND_1_TICK_GAP, -1)  # reshape to (28800/2, 20)
-    lob_data_1_second_df = pd.DataFrame(lob_data_1_second, columns=LOB_COLUMNS)  # to df
+    lob_data_1_second_df = pd.DataFrame(lob_data_1_second, columns=LOB_COLUMNS)  # construct the df
     lob_data_1_second_df.to_csv(f"{LOB_1_SECOND_FILE_PATH}/1_second_{lob_file}", index=False)
     print(f"   - 2. FINISH 1 SECOND !!!")
 
-    # - 2.3 compute the 10 seconds lob data
-    # define the empty 10-second lob data, shape=(28800/20, 5, 2, 2)
+    # - 2.3 compute the 10 seconds lob data, align by price during 20 ticks and sum volume
+    # define the empty 10-second lob data, shape=(28800/20, 5, 2, 2), (T, L, D, F)
     lob_data_10_second = np.zeros((TICK_NUM // SECOND_10_TICK_GAP, 5, 2, 2))
-    # for loop the tick by `HF` to gen all PA Feature of each tick
+    # for loop the tick by `SECOND_10_TICK_GAP` to gen all PA Feature of each tick
+    tick_i = 0
     for tick_i in range(SECOND_10_TICK_GAP - 1, TICK_NUM, SECOND_10_TICK_GAP):
         # compute the price range and paf-volume
         bid_price_range, bid_paf_volume = cal_bid_paf_mat(
@@ -118,15 +132,17 @@ for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granulari
         lob_data_10_second[tick_i // SECOND_10_TICK_GAP, :, 0, 1] = bid_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
         lob_data_10_second[tick_i // SECOND_10_TICK_GAP, :, 1, 0] = ask_price_range[:5]  # bid price
         lob_data_10_second[tick_i // SECOND_10_TICK_GAP, :, 1, 1] = ask_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
+    assert tick_i == TICK_NUM - 1, "tick_i ERROR !"  # test the tick_i
     lob_data_10_second = lob_data_10_second.reshape(TICK_NUM // SECOND_10_TICK_GAP, -1)  # reshape to (28800/20, 20)
-    lob_data_10_second_df = pd.DataFrame(lob_data_10_second, columns=LOB_COLUMNS)  # to df
-    lob_data_10_second_df.to_csv(f"{LOB_10_SECOND_FILE_PATH}/10_seoends_{lob_file}", index=False)
+    lob_data_10_second_df = pd.DataFrame(lob_data_10_second, columns=LOB_COLUMNS)  # construct the df
+    lob_data_10_second_df.to_csv(f"{LOB_10_SECOND_FILE_PATH}/10_seconds_{lob_file}", index=False)
     print(f"   - 3. FINISH 10 SECONDS !!!")
 
-    # - 2.4 compute the 30 seconds lob data
-    # define the empty 30-second lob data, shape=(28800/60, 5, 2, 2)
+    # - 2.4 compute the 30 seconds lob data, align by price during 60 ticks and sum volume
+    # define the empty 30-second lob data, shape=(28800/60, 5, 2, 2), (T, L, D, F)
     lob_data_30_second = np.zeros((TICK_NUM // SECOND_30_TICK_GAP, 5, 2, 2))
     # for loop the tick by `HF` to gen all PA Feature of each tick
+    tick_i = 0
     for tick_i in range(SECOND_30_TICK_GAP - 1, TICK_NUM, SECOND_30_TICK_GAP):
         # compute the price range and paf-volume
         bid_price_range, bid_paf_volume = cal_bid_paf_mat(
@@ -142,15 +158,17 @@ for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granulari
         lob_data_30_second[tick_i // SECOND_30_TICK_GAP, :, 0, 1] = bid_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
         lob_data_30_second[tick_i // SECOND_30_TICK_GAP, :, 1, 0] = ask_price_range[:5]  # bid price
         lob_data_30_second[tick_i // SECOND_30_TICK_GAP, :, 1, 1] = ask_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
+    assert tick_i == TICK_NUM - 1, "tick_i ERROR !"  # test the tick_i
     lob_data_30_second = lob_data_30_second.reshape(TICK_NUM // SECOND_30_TICK_GAP, -1)  # reshape to (28800/20, 20)
     lob_data_30_second = pd.DataFrame(lob_data_30_second, columns=LOB_COLUMNS)  # to df
     lob_data_30_second.to_csv(f"{LOB_30_SECOND_FILE_PATH}/30_seconds_{lob_file}", index=False)
     print(f"   - 4. FINISH 30 SECONDS !!!")
 
-    # - 2.5 compute the 1 minute lob data
-    # define the empty 1-minute lob data, shape=(28800/120, 5, 2, 2)
+    # - 2.5 compute the 1-minute lob data, align by price during 120 ticks and sum volume
+    # define the empty 1-minute lob data, shape=(28800/120, 5, 2, 2), (T, L, D, F)
     lob_data_1_minute = np.zeros((TICK_NUM // MINUTE_1_TICK_GAP, 5, 2, 2))
-    # for loop the tick by `HF` to gen all PA Feature of each tick
+    # for loop the tick by `MINUTE_1_TICK_GAP` to gen all PA Feature of each tick
+    tick_i = 0
     for tick_i in range(MINUTE_1_TICK_GAP - 1, TICK_NUM, MINUTE_1_TICK_GAP):
         # compute the price range and paf-volume
         bid_price_range, bid_paf_volume = cal_bid_paf_mat(
@@ -166,9 +184,9 @@ for file_idx, lob_file in enumerate(lob_file_list):  # Computing other granulari
         lob_data_1_minute[tick_i // MINUTE_1_TICK_GAP, :, 0, 1] = bid_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
         lob_data_1_minute[tick_i // MINUTE_1_TICK_GAP, :, 1, 0] = ask_price_range[:5]  # bid price
         lob_data_1_minute[tick_i // MINUTE_1_TICK_GAP, :, 1, 1] = ask_paf_volume.sum(axis=(0, 2))[:5]  # bid volume
+    assert tick_i == TICK_NUM - 1, "tick_i ERROR !"  # test the tick_i
     lob_data_1_minute = lob_data_1_minute.reshape(TICK_NUM // MINUTE_1_TICK_GAP, -1)  # reshape to (28800/20, 20)
     lob_data_1_minute = pd.DataFrame(lob_data_1_minute, columns=LOB_COLUMNS)  # to df
     lob_data_1_minute.to_csv(f"{LOB_1_MINUTE_FILE_PATH}/1_minute_{lob_file}", index=False)
     print(f"   - 5. FINISH 1 MINUTE !!!")
-
     break
