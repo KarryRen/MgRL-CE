47da2efe7658e753b027377d0b9d946986ffca39
KarryRen <KarryRenKai@outlook.com>
2024-04-17 13:00:24 +0800

    add: add 2 config files

---
+++ configs/index_config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/17 12:59
+# @Author  : Karry Ren
+
+""""""
---
+++ configs/lob_config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2024/4/17 13:00
+# @Author  : Karry Ren
+
+""""""
